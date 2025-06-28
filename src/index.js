00cccde34a3f213cd1c861c221beec99949293b0
gustavogss <gustavogss.dev@gmail.com>
2025-06-28 14:52:46 -0300

    feat: sort block

--- src/index.js
+++ src/index.js
@@ -50,8 +50,35 @@ async function  rollDice() {
   return  Math.floor(Math.random()*6 + 1)
 }
 
+async function getRandomBlock() {
+  let random = Math.random()
+  let result = ""
+  switch (true) {
+    case random < 0.33:
+      result = "RETA"
+      break;
+    case random < 0.66:
+      result = "CURVA"
+      break;
+    default:
+       result = "CONFRONTO"
+  }
+  return result
+}
+
+async function playRaceEngine(character1, character2) {
+  for (let round = 1; round < 5; round++) {
+    console.log(`🏁 Rodada ${round}`);    
+    
+    let block = await getRandomBlock() 
+      console.log(`Bloco: ${block}`)      
+    }    
+  }
+
 (async function main() {
   console.log(`
     🚨 🏁 Corrida entre o ${player1.nome} e o ${player5.nome} começando... \n
     `);  
+
+  await playRaceEngine(player1, player5)
 })()
